b9450a6626e0586a2e29e777822cceea9a2d86ea
Rahul Kuiry <rahulkuiry04@gmail.com>
2026-09-04 20:22:10 +0530

    app added

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
 from Tools.tavily_tool import tavily_search
+from Tools.flight_tool import search_flights
+from backend import run_travel_agent
+
 # res=tavily_search("Best places to visit in Europe")
 # print(res)
-from Tools.flight_tool import search_flights
-res=search_flights("Plan a 7 days Nepal trip from India")
-print(res)
+# from Tools.flight_tool import search_flights
+# res=search_flights("Plan a 7 days Nepal trip from India")
+# print(res)
+user_input = input("Enter travel request: ")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
+
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])
